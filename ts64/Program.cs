8c0cca30c3738e31807c0cac6758d5456e223214
deterquetejoder <saecularisator@gmail.com>
2023-09-15 22:20:33 +0500

    works

--- ts64/Program.cs
+++ ts64/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет
+//  все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.     
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = Input("Enter a number: ");
+        RecursivePrint(N);
+    }
+
+    static int Input(string msg)
+    {
+        Console.WriteLine(msg);
+        int res = Convert.ToInt32(Console.ReadLine());
+        return res;
+    }
+
+    static void RecursivePrint(int number)
+    {
+        if (number > 1)
+        {
+            Console.WriteLine(number);
+            RecursivePrint(number - 1);
+        }
+        else
+        {
+            Console.WriteLine("1");
+        }
+    }
+}
