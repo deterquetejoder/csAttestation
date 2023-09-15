f4f0c84d4bfeeeaf194b7687d74792b9f5172f84
deterquetejoder <saecularisator@gmail.com>
2023-09-15 22:30:25 +0500

    doesnt work but interesting

--- ts66/Program.cs
+++ ts66/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static int Sum(int m, int n)
+    {
+        if (m == n)
+            return n;
+        else
+            return m + Sum(m + 1, n);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter M and N:");
+        string input = Console.ReadLine();
+        int m = Convert.ToInt32(input.Split(' ')[0]);
+        int n = Convert.ToInt32(input.Split(' ')[1]);
+        Console.WriteLine($"The sum of natural numbers from {m} to {n} is {Sum(m, n)}.");
+    }
+}
